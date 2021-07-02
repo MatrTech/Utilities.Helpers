dc5a63a4030572b697aa0b060015f1bd85bf1382
Mark Visschers <mark.visschers@connecthing.tech>
2021-07-02 16:30:46 +0200

    initial and activator helper

---
+++ src/ActivatorHelper/ActivatorHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MatrTech.Utilities.Helpers
+{
+    public static class ActivatorHelper
+    {
+#if NETCOREAPP3_1
+        public static T CreateInstance<T>(params object?[]? parameters)
+            where T : class
+        {
+            return (T?)Activator.CreateInstance(typeof(T), parameters);
+        }
+#else
+
+        public static T? CreateInstance<T>(params object?[]? parameters)
+        {
+            return (T?)Activator.CreateInstance(typeof(T), parameters);
+        }
+
+#endif
+    }
+}
---
+++ tests/ActivatorHelperTests/ActivatorHelperTests.cs
@@ -0,0 +1,43 @@
+using FluentAssertions;
+using MatrTech.Utilities.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ActivatorHelperTests
+{
+    [TestClass]
+    public class ActivatorHelperTests
+    {
+        [TestMethod]
+        public void CreateInstance_WithoutParameters_NotBeNull()
+        {
+            ActivatorHelper.CreateInstance<TestClassWithoutParameters>()
+                .Should()
+                .NotBeNull();
+        }
+
+        [TestMethod]
+        public void CreateInstance_WithParameters_NotBeNull()
+        {
+            ActivatorHelper.CreateInstance<TestClassWithParameters>(3)
+                .Should()
+                .NotBeNull();
+        }
+
+        [TestMethod]
+        public void CreateInstance_WithParametersButNotProvided_ShouldThrowException()
+        {
+            Func<TestClassWithParameters> func = () => ActivatorHelper.CreateInstance<TestClassWithParameters>();
+            func.Should().Throw<MissingMethodException>();
+        }
+
+        private class TestClassWithoutParameters { }
+
+        private class TestClassWithParameters
+        {
+            public TestClassWithParameters(int _)
+            {
+            }
+        }
+    }
+}
